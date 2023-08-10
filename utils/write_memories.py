15b0f64a621ed3b5f009511b45bab7bbece6d871
neqonagomiya <neqofan@gmail.com>
2023-08-11 06:51:06 +0900

    feature: chatGPTに日記のタイトルも一緒に書いてもらうwrite_memories_with_titleを実装（その中で利用するremove_space, pickup_contentsも実装）#3 #4

--- utils/write_memories.py
+++ utils/write_memories.py
@@ -1,5 +1,5 @@
 import os
-
+import re
 import openai
 
 class WriterGPT:
@@ -44,7 +44,22 @@ class WriterGPT:
                 ],
             )
         return {"answer": res.choices[0]["message"]["content"].strip()}
-        
+
+    def remove_space(self, sentence:str)->str:
+        return sentence.replace(" ", "")
+
+    def pickup_contents(self, sentence:str, label:str, end_flag=False)-> str:
+        if end_flag == True:
+            pickup_pattern = r"" + label + r"(.*)$"
+        else:
+            pickup_pattern = r"" + label + r"(.*)\n"
+
+        match = re.search(pickup_pattern, sentence)
+        if match:
+            pickuped_sentence = match.group(1)
+        else:
+            pickuped_sentence = None
+        return pickuped_sentence
 
     def write_memories(self, facilities_list):
         facilities_str = self.facilitieslist2str(facilities_list)
@@ -72,3 +87,52 @@ class WriterGPT:
                 )
         response = writer.choices[0].message.content
         return response
+
+    def write_memories_with_title(self, facilities_list):
+        """
+        you need in main.py:
+            - class DiaryWtModel(BaseModel)
+                title: str
+                diary: str
+            
+        """
+        facilities_str = self.facilitieslist2str(facilities_list)
+        self.prompt = f"""
+        以下の条件に基づいて、お出かけの日記を書いてください\n
+
+        # 執筆ルール\n
+        - titleは30文字以内で書いてください、そして改行はしないでください
+        - diaryは150文字以内で書いてください、そして改行はしないでください
+        - 以下で決められた「出力形式」に従う\n
+
+        # お出かけで行った場所\n
+        {facilities_str}
+
+        # 出力形式
+
+        title:「#お出かけで行った場所」の要素から考えた、お出かけの素敵な概要
+        diary:「#お出かけで行った場所」の内容を用いた日記
+
+        """.replace(" ", "")
+
+        # 「素敵な」をつけるだけで全然違う
+        # title: 「お出かけで行った場所」からお出かけのタイトルを考えて[title]に設定する
+        # diary: 「お出かけで行った場所」を使って、日記を考えて[diary]に設定する
+
+        print(self.prompt)
+        writer = openai.ChatCompletion.create(
+                    model=self.writer_model,
+                    messages=[
+                        {"role": "system", "content": self.system_role},
+                        {"role": "user", "content": self.prompt},
+                    ],
+                    max_tokens=self.max_tokens,
+                    n = self.response_num,
+                    stop=self.stop_word,
+                    temperature=self.response_randomness
+                )
+        response = writer.choices[0].message.content
+        response_nospace = self.remove_space(response)
+        title_content = self.pickup_contents(response_nospace, "title:")
+        diary_content = self.pickup_contents(response_nospace, "diary:", end_flag=True)
+        return {"title": title_content, "diary": diary_content}
